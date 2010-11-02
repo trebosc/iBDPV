cdd30ac8cd605a29171063134b36644d56cb53ee
David TREBOSC <david.trebosc@macoda.com>
2010-11-02 17:25:47 +0100

    2 fichiers manquants lors du dernier push

---
+++ Classes/AproposViewController.h
@@ -0,0 +1,19 @@
+//
+//  GenericViewController.h
+//  iBDPV
+//
+//  Created by jmd on 01/08/10.
+//  Copyright 2010 __MaCoDa__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AproposViewController : UIViewController {
+
+}
+
+//Action Back
+-(void)actBack:(id)sender;
+
+@end
---
+++ Classes/AproposViewController.m
@@ -0,0 +1,138 @@
+    //
+//  AproposViewController.m
+//  iBDPV
+//
+//  Created by jmd on 01/08/10.
+//  Copyright 2010 __MaCoDa__. All rights reserved.
+//
+
+#import "AproposViewController.h"
+
+
+@implementation AproposViewController
+
+/*
+ //-------------------------------------------------------------------------------------------------------------------------------
+ // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
+        // Custom initialization
+    }
+    return self;
+}
+*/
+
+
+//-------------------------------------------------------------------------------------------------------------------------------
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView {
+
+	// Affichage de la barre de navigation
+	[self.navigationController setNavigationBarHidden:NO];
+	
+	//Titre
+	self.title=@"A propos";
+	
+	//Désactivation du bouton Back
+	[self.navigationItem setHidesBackButton:YES];
+
+	//Affichage de la toolBar du Navigation Controller
+	[self.navigationController setToolbarHidden:NO animated:YES];
+	
+	//Création des boutons
+	//Retour
+	UIBarButtonItem *btnBackItem=[[UIBarButtonItem alloc]initWithTitle:@"Retour" style:UIBarButtonItemStyleBordered target:self action:@selector(actBack:)];
+	//Espacement
+
+	// Ajout des boutons dans la toolBar
+	self.toolbarItems=[NSArray arrayWithObjects:btnBackItem,nil];
+
+	[btnBackItem release];
+	
+    //Création de la hiérarchie des Views
+    // 1. Création de la vue racine du controlleur de la taille de l'écran
+	// Background
+    UIImageView *rootView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"fond_menu.png"]];
+    rootView.userInteractionEnabled=YES;    //NO by default and YES for a UIView
+    rootView.opaque = YES;
+    
+	// 2. Ajout de subViews
+	CGRect lblRect=CGRectMake(50.0, 50.0, 200, 40);
+	UILabel	*lblWelcome=[[UILabel alloc] initWithFrame:lblRect];
+	lblWelcome.text=@"A Propos View Controller";
+	[rootView addSubview:lblWelcome];
+	[lblWelcome release];
+	
+	
+	
+	// 3. Assignation de la vue racine à la propriété view du controlleur
+	self.view=rootView;
+	
+	// 4. Libération de la vue racine
+	[rootView release];
+    
+    
+	
+}
+
+/*
+ //-------------------------------------------------------------------------------------------------------------------------------
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    //[super viewDidLoad];
+	
+	NSLog(@"viewDidLoad: AProposViewController");
+	
+	// Création par programme de la hiérarchie de vues (p34) 
+		
+
+}
+/*
+
+/*
+ //-------------------------------------------------------------------------------------------------------------------------------
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+//-------------------------------------------------------------------------------------------------------------------------------
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------------------
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+//#########################################################################################################################################################
+//#########################################################################################################################################################
+#pragma mark -
+#pragma mark === Actions ===
+//-------------------------------------------------------------------------------------------------------------------------------
+//Action Back
+-(void)actBack:(id)sender {
+			NSLog(@"Top: @%",self.navigationController.topViewController);
+	
+	//Retour au controlleur précédent
+	[self.navigationController popViewControllerAnimated:YES];
+	//[self.navigationController dismissModalViewControllerAnimated:YES];
+}
+
+
+@end
